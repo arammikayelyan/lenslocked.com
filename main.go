2070e95bebae3cc341fb395eea036f6289bfce67
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2020-04-16 15:55:10 +0400

    Created web server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome!</h1>")
 }
